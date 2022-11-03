32075d2d2e0d53320a1be8ab905a5c22b118d52a
SergeiIgonin <107101969+SergeiIgonin@users.noreply.github.com>
2022-11-03 09:10:31 +0400

    Pytest Проверка ожидаемого результата

--- lesson33_step9_test.py
+++ lesson33_step9_test.py
@@ -1,9 +1,7 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
 
 def test_exception1():
     try:
